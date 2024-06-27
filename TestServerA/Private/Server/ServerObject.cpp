4ea1b8b41f8144971a2665c01c003b5aec883260
JinXin <741799837@qq.com>
2024-06-27 12:58:57 +0800

    C服务器向A服务器发送消息，A服务器利用SIMPLE_CLIENT_SEND将这个消息转发给B服务器，一定要获取地址FSimpleAddrInfo

--- TestServerA/Private/Server/ServerObject.cpp
+++ TestServerA/Private/Server/ServerObject.cpp
@@ -1,6 +1,7 @@
 #include "ServerObject.h"
 #include "Protocols/TestServerProtocols.h"
 #include "TestServerALog.h"
+#include "TestServerList.h"
 
 void UServerObject::Init()
 {
@@ -23,21 +24,26 @@ void UServerObject::RecvProtocol(uint32 InProtocol)
 	//接收其他服务器里的客户端发送过来的协议
 	switch (InProtocol)
 	{
-	case SP_HelloRequests:
-		FString HelloString;
-		int32 vvv = INDEX_NONE;
-		SIMPLE_PROTOCOLS_RECEIVE(SP_HelloRequests, HelloString, vvv);
+		case SP_HelloRequests:
+			FString HelloString;
+			int32 vvv = INDEX_NONE;
+			SIMPLE_PROTOCOLS_RECEIVE(SP_HelloRequests, HelloString, vvv);
 
-		if (!HelloString.IsEmpty())
-		{
-			UE_LOG(LogTestServerA, Display, TEXT("AServer Recv %s %i"), *HelloString, vvv);
+			if (!HelloString.IsEmpty())
+			{
+				UE_LOG(LogTestServerA, Display, TEXT("AServer Recv %s %i"), *HelloString, vvv);
 
-			//协议回调
-			FString HelloResp = TEXT("OK!");
-			SIMPLE_PROTOCOLS_SEND(SP_HelloResponses, HelloResp);
-			UE_LOG(LogTestServerA, Display, TEXT("AServer send %s"), *HelloResp);
-		}
-		break;
+			
+				FString HelloResp = TEXT("OK!");
+				//协议回调(当接收到C服务器中的A客户端向A服务器发送的协议时，通过A服务器直接发送协议回复给C服务器中的A客户端)
+				//SIMPLE_PROTOCOLS_SEND(SP_HelloResponses, HelloResp);
+				//协议转发,一定要记录地址(当A服务器接收到C服务器中的A客户端发送的协议时，将这个协议通过A服务器中的B客户端转发给B服务器)
+				FSimpleAddrInfo AddrInfo;
+				GetAddrInfo(AddrInfo);
+				SIMPLE_CLIENT_SEND(BClient, SP_HelloRequests, HelloString, AddrInfo)
+				UE_LOG(LogTestServerA, Display, TEXT("AServer send %s"), *HelloResp);
+			}
+			break;
 	}
 }
 
--- TestServerB/Private/Server/ServerObject.cpp
+++ TestServerB/Private/Server/ServerObject.cpp
@@ -1,4 +1,6 @@
 #include "ServerObject.h"
+#include "Protocols/TestServerProtocols.h"
+#include "TestServerBLog.h"
 
 void UServerObject::Init()
 {
@@ -18,5 +20,20 @@ void UServerObject::Close()
 void UServerObject::RecvProtocol(uint32 InProtocol)
 {
 	Super::RecvProtocol(InProtocol);
+
+	switch (InProtocol)
+	{
+		case SP_HelloRequests:
+			FString HelloString;
+			FSimpleAddrInfo AddrInfo;
+			SIMPLE_PROTOCOLS_RECEIVE(SP_HelloRequests, HelloString, AddrInfo);
+
+			if (!HelloString.IsEmpty())
+			{
+				UE_LOG(LogTestServerB, Display, TEXT("BServer Recv %s"), *HelloString);
+			}
+
+			break;
+	}
 }
 
