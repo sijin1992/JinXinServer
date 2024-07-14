bc1267efdea5d2b0662b091699322a7e6ff0ff50
JinXin <741799837@qq.com>
2024-07-14 10:35:43 +0800

    登录服务器： 分离日志

--- MMORPGLoginServer/Private/MMORPGLoginServer.cpp
+++ MMORPGLoginServer/Private/MMORPGLoginServer.cpp
@@ -2,17 +2,15 @@
 
 
 #include "MMORPGLoginServer.h"
-
+#include "Log/MMORPGLoginServerLog.h"
 #include "RequiredProgramMainCPPInclude.h"
-
-DEFINE_LOG_CATEGORY_STATIC(LogMMORPGLoginServer, Log, All);
 
 IMPLEMENT_APPLICATION(MMORPGLoginServer, "MMORPGLoginServer");
 
 INT32_MAIN_INT32_ARGC_TCHAR_ARGV()
 {
 	GEngineLoop.PreInit(ArgC, ArgV);
-	UE_LOG(LogMMORPGLoginServer, Display, TEXT("Hello World"));
+	UE_LOG(LogMMORPGLoginServer, Display, TEXT("MMORPGLoginServer Beginning"));
 	FEngineLoop::AppExit();
 	return 0;
 }
